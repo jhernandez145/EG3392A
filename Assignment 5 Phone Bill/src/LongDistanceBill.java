f74604208c55f0a730edc76d4bb992745f54f700
Jesus Hernandez <jesusahdz96@hotmail.com>
2016-09-22 22:40:51 -0500

    Initial commit

---
+++ Assignment 5 Phone Bill/src/LongDistanceBill.java
@@ -0,0 +1,28 @@
+
+public class LongDistanceBill extends PhoneBill {
+	private double callRate;
+	
+	public LongDistanceBill() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public LongDistanceBill(double monthlyRate) {
+		super(monthlyRate);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public double getCallRate() {
+		return callRate;
+	}
+
+	public void setCallRate(double callRate) {
+		this.callRate = callRate;
+	}
+
+	@Override
+	public double calculateBill(int minutesTalked) {
+		// TODO Auto-generated method stub
+		return ((callRate * minutesTalked) + getMonthlyRate());
+	}
+
+}
---
+++ Assignment 5 Phone Bill/src/TestPhoneBill.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+import java.text.NumberFormat;
+import java.util.Locale;
+public class TestPhoneBill {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		LongDistanceBill ldBill = new LongDistanceBill();
+		
+		double callRate, monthlyRate; 
+		int minutesTalked;
+		
+		System.out.println("This program will calculate your phone bill for you");
+		
+		System.out.println("What is your phone rate?");
+		callRate = input.nextDouble();
+		
+		System.out.println("What is your monthly rate?");
+		monthlyRate = input.nextDouble();
+		
+		System.out.println("How manu minutes did you talk?");
+		minutesTalked = input.nextInt();
+		
+		ldBill.setCallRate(callRate);
+		ldBill.setMonthlyRate(monthlyRate);
+		System.out.println("Your monthly bill is " + NumberFormat.getCurrencyInstance(new Locale("en", "US")).format(ldBill.calculateBill(minutesTalked)));
+		
+	}
+
+}
