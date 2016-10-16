7d077cb93b18e648238af856ebfe53e281f25b29
Jesus Hernandez <jesusahdz96@hotmail.com>
2016-10-16 01:17:38 -0500

    Changed code to use inheritance

--- Assignment 8 ColorPanels/src/ColorPanels.java
+++ Assignment 8 ColorPanels/src/ColorPanels.java
@@ -1,113 +1,97 @@
-/*
- * Jesus Hernandez 614019
- * 10-12-19
- * Assignment 8 Question 2
- */
-
 import javax.swing.*;
 import java.awt.Color;
 
-public class ColorPanels {
-	final static String TITLE = "Color Panels";
-	final static String RED_LABEL = "Red";
-	final static String BLUE_LABEL = "Blue";
-	final static String YELLOW_LABEL = "Yellow";
-	
-	final static int WINDOW_WIDTH = 1000;
-	final static int WINDOW_HEIGHT = 500;
-	
-	final static int PANEL_WIDTH = (WINDOW_WIDTH / 4);
-	final static int PANEL_HEIGHT = (WINDOW_HEIGHT / 2);
-	final static int PANEL_VERTICLE = 100;
-	final static int PANEL_HORIZONTAL = 20;
-	
+public class ColorPanels extends JFrame {
+	private static final long serialVersionUID = 1L;
+
+	public ColorPanels() {
+		setLayout(null);
+		setSize(1200, 600);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setTitle("Colors Panel");
+		setResizable(false);
+		setVisible(true);
+		setLocationRelativeTo(null);
+	}
+
 	public static void main(String[] args) {
-		/*
-		 *  CREATE ALL THE SWING OBJECTS 
-		 */
-		
-		// JFrame
-		JFrame frame = new JFrame(TITLE);
-		
-		// JPanels
-		JPanel rPanel = new JPanel();
-		JPanel bPanel = new JPanel();
-		JPanel yPanel = new JPanel();
-		
-		// JLabels
-		JLabel rLabel = new JLabel(RED_LABEL);
-		JLabel bLabel = new JLabel(BLUE_LABEL);
-		JLabel yLabel = new JLabel(YELLOW_LABEL);
-		
-		// JButtons
-		JButton rButton = new JButton(RED_LABEL.toUpperCase());
-		JButton bButton = new JButton(BLUE_LABEL.toUpperCase());
-		JButton yButton = new JButton(YELLOW_LABEL.toUpperCase());
-		
-		// Set panel properties
-		setPanelProperties(rPanel, Color.RED, 0);
-		setPanelProperties(bPanel, Color.BLUE, 1);
-		setPanelProperties(yPanel, Color.YELLOW, 2);
-		
-		// Set label properties
-		setLabelProperties(rLabel, Color.RED, 0);
-		setLabelProperties(bLabel, Color.BLUE, 1);
-		setLabelProperties(yLabel, Color.YELLOW, 2);
-		
-		// Set button properties
-		setButtonProperties(rButton, 0);
-		setButtonProperties(bButton, 1);
-		setButtonProperties(yButton, 2);
-		
-		/*
-		 *  ADD PANELS/LABELS/BUTTONS AND SET THE FRAME PROPERTIES
-		 */
-		
-		// Add panels
-		frame.add(rPanel);
-		frame.add(bPanel);
-		frame.add(yPanel);
-		
-		// Add labels
-		frame.add(rLabel);
-		frame.add(bLabel);
-		frame.add(yLabel);
-		
-		// Add buttons
-		frame.add(rButton);
-		frame.add(bButton);
-		frame.add(yButton);
-		
-		// Set JFrame properties
-		setFrameProperties(frame);
+		ColorPanels frame = new ColorPanels();
+
+		JPanel panel_0 = new JPanel(null);
+		setParentPanelProperties(frame, panel_0, Color.LIGHT_GRAY, 0);
+
+		JPanel panel_1 = new JPanel(null);
+		setParentPanelProperties(frame, panel_1, Color.LIGHT_GRAY, 1);
+
+		JPanel panel_2 = new JPanel(null);
+		setParentPanelProperties(frame, panel_2, Color.LIGHT_GRAY, 2);
+
+		JPanel rPanel = new JPanel(null);
+		setChildPanelProperties(panel_0, rPanel, Color.RED);
+
+		JPanel bPanel = new JPanel(null);
+		setChildPanelProperties(panel_1, bPanel, Color.BLUE);
+
+		JPanel yPanel = new JPanel(null);
+		setChildPanelProperties(panel_2, yPanel, Color.YELLOW);
+
+		JLabel rLabel = new JLabel("Red");
+		setLabelProperties(rPanel, rLabel, Color.RED);
+
+		JLabel bLabel = new JLabel("Blue");
+		setLabelProperties(bPanel, bLabel, Color.BLUE);
+
+		JLabel yLabel = new JLabel("Yellow");
+		setLabelProperties(yPanel, yLabel, Color.YELLOW);
+
+		JButton rButton = new JButton("RED");
+		setButtonProperties(rPanel, rButton);
+
+		JButton bButton = new JButton("BLUE");
+		setButtonProperties(bPanel, bButton);
+
+		JButton yButton = new JButton("YELLOW");
+		setButtonProperties(yPanel, yButton);
+
+		panel_0.add(rPanel);
+		panel_1.add(bPanel);
+		panel_2.add(yPanel);
+
+		panel_0.add(rLabel);
+		panel_1.add(bLabel);
+		panel_2.add(yLabel);
+
+		panel_0.add(rButton);
+		panel_1.add(bButton);
+		panel_2.add(yButton);
+
+		frame.getContentPane().add(panel_0);
+		frame.getContentPane().add(panel_1);
+		frame.getContentPane().add(panel_2);
 	}
-	public static void setPanelProperties(JPanel panel, Color color, int offset){
-		int horizontalOffset = (offset * (250 + (250 / 2) - 25));
-		panel.setLayout(null);
+
+	public static void setParentPanelProperties(JFrame frame, JPanel panel, Color color, int location) {
 		panel.setBackground(color);
-		panel.setLocation(PANEL_HORIZONTAL + horizontalOffset, PANEL_VERTICLE);
-		panel.setSize(PANEL_WIDTH, PANEL_HEIGHT);
+		panel.setBounds(((frame.getWidth() / 3) * location), 0, (frame.getWidth() / 3), frame.getHeight());
 		panel.setVisible(true);
-		
 	}
-	public static void setLabelProperties(JLabel label, Color color, int offset){
-		int horizontalOffset = ((offset * (250 + (250 / 2) - 25)) + 115);	// lableHorizontalOffset + 115
-		label.setLayout(null);
+
+	public static void setChildPanelProperties(JPanel parentPanel, JPanel childPanel, Color color) {
+		childPanel.setBounds((parentPanel.getWidth() / 3), (parentPanel.getHeight() / 3), (parentPanel.getHeight() / 4),
+				(parentPanel.getWidth() / 4));
+		childPanel.setBackground(color);
+		childPanel.setVisible(true);
+	}
+
+	public static void setLabelProperties(JPanel panel, JLabel label, Color color) {
+		label.setBounds((panel.getX() + (panel.getWidth() / 3) + 3), (panel.getY() - panel.getHeight()), 100, 100);
 		label.setForeground(color);
-		label.setBounds(PANEL_HORIZONTAL + horizontalOffset, PANEL_VERTICLE - 150, PANEL_WIDTH, PANEL_HEIGHT);	//No clue why I have to subtract from vertical to go up
 		label.setVisible(true);
 	}
-	public static void setButtonProperties(JButton button, int offset){
-		int horizontalOffset = ((offset * (250 + (250 / 2) - 25)) + 80);	//labelHorizontalOffset + 80
-		button.setLayout(null);
-		button.setBounds(PANEL_HORIZONTAL + horizontalOffset, PANEL_VERTICLE + 300 , 100, 50);
+
+	public static void setButtonProperties(JPanel panel, JButton button) {
+		button.setEnabled(true);
+		button.setBounds((panel.getX() + (panel.getWidth() / 6)), panel.getHeight() + panel.getY() + 50, 100, 50);
 		button.setVisible(true);
-	}
-	public static void setFrameProperties(JFrame frame){
-		frame.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
-		frame.setLocationRelativeTo(null);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setLayout(null);
-		frame.setVisible(true);
 	}
 }
